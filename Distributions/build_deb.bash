7bc8387c3da96aa2c78654a41464d6c7b8ddfdcb
textbrowser <textbrowser@gmail.com>
2022-05-12 12:18:33 -0400

    Debian.

---
+++ Distributions/build_deb.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+mkdir -p ./opt/dreamy
+qmake -o Makefile dreamy.pro && make -j $(nproc)
+cp -p ./Dreamy ./opt/dreamy/.
+mkdir -p dreamy-debian.d/opt
+cp -pr DEBIAN dreamy-debian.d/.
+cp -r ./opt/dreamy dreamy-debian.d/opt/.
+fakeroot dpkg-deb --build dreamy-debian.d Dreamy-2022.05.15_amd64.deb
+make distclean
+rm -fr ./opt
+rm -fr dreamy-debian.d
