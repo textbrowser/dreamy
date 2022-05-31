1218572a806010baa5e4571cf7db1d2f26af58aa
textbrowser <textbrowser@gmail.com>
2022-05-30 20:10:58 -0400

    New version.

--- Distributions/build_deb.bash
+++ Distributions/build_deb.bash
@@ -6,7 +6,7 @@ cp -p ./Dreamy ./opt/dreamy/.
 mkdir -p dreamy-debian.d/opt
 cp -pr DEBIAN dreamy-debian.d/.
 cp -r ./opt/dreamy dreamy-debian.d/opt/.
-fakeroot dpkg-deb -Zgzip --build dreamy-debian.d Dreamy-2022.05.15_amd64.deb
+fakeroot dpkg-deb -Zgzip --build dreamy-debian.d Dreamy-2022.05.30_amd64.deb
 make distclean
 rm -fr ./opt
 rm -fr dreamy-debian.d
--- Source/dreamy_version.h
+++ Source/dreamy_version.h
@@ -28,6 +28,6 @@
 #ifndef _dreamy_version_h_
 #define _dreamy_version_h_
 
-#define DREAMY_VERSION "2022.05.20"
+#define DREAMY_VERSION "2022.05.30"
 
 #endif
